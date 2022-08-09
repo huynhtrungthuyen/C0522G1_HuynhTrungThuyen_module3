bbb8e9f3aaebda84d4d10ec4a34fa4a81dcee707
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-08-09 20:55:36 +0700

    cs2

--- ss01_database_introduction/exercise/student_management.sql
+++ ss01_database_introduction/exercise/student_management.sql
@@ -4,19 +4,19 @@ create database student_management;
 use student_management;
 
 create table student(
-id int primary key not null,
+id int primary key,
 `name` varchar(45) not null,
 age int,
 country varchar(45)
 );
 
 create table class(
-id int primary key not null,
+id int primary key,
 `name` varchar(45) not null
 );
 
 create table teacher(
-id int primary key not null,
+id int primary key,
 `name` varchar(45) not null,
 age int,
 country varchar(45)
