f23c6527104f939d7270bbf5b960ad0c9bd75e4f
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-08-12 21:41:37 +0700

    cs20

--- ss05_view_index_stored_procedure/exercise/products.sql
+++ ss05_view_index_stored_procedure/exercise/products.sql
@@ -48,12 +48,48 @@ select * from products;
 -- tiến hành xoá view
 drop view w_products;
 
--- Tạo store procedure lấy tất cả thông tin của tất cả các sản phẩm trong bảng product
+-- Tạo store procedure lấy tất cả thông tin của tất cả các sản phẩm trong bảng product.
 delimiter //
 create procedure sp_get_info_products()
 begin
     select * from products;
 end // 
 delimiter ;
+
+call sp_get_info_products();
+
+-- Tạo store procedure thêm một sản phẩm mới.
+delimiter //
+create procedure sp_add_product (in p_id int, in p_code int, in p_name varchar(45), in p_price int, in p_amount int, in p_desciption text, in p_status bit)
+begin
+    insert into products values (p_id, p_code, p_name, p_price, p_amount, p_desciption, p_status);
+end //
+delimiter ;
+
+call sp_add_product(6, 6, 'Thanh long không hạt', 10000, 60, 'Đố bạn tìm ra hột.', 1);
+
+call sp_get_info_products();
+
+-- Tạo store procedure sửa thông tin sản phẩm theo id.
+delimiter //
+create procedure sp_set_produc_by_id (in id_set int, in product_name_update varchar(45))
+begin
+    update products set product_name = product_name_update where id = id_set;
+end //
+delimiter ;
+
+call sp_set_produc_by_id(6, 'Thanh long không hột');
+
+call sp_get_info_products();
+
+-- Tạo store procedure xoá sản phẩm theo id.
+delimiter //
+create procedure sp_remove_produc_by_id (in id_delete int)
+begin
+    delete from products where id = id_delete;
+end //
+delimiter ;
+
+call sp_remove_produc_by_id(6);
 
 call sp_get_info_products();
